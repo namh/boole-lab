9941d8825d1913d9ed5a3509179b74402ac465b9
Nam <voidx288@yahoo.com>
2011-02-14 13:40:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonsai::Application.config.secret_token = 'f34ca6f978239f56a142fa196dd6d80d0882885afce4807e00f7eeba33c9cdc9c4438a95068cf3f0e6ad2b50f4559fec4220d42a8e575d45df55b82f709fac90'
